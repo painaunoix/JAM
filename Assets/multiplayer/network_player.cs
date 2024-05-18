1f6a38a2a63fe496e22437b1049f2cbe8bdbb489
nashiii <146207733+maxNashiii@users.noreply.github.com>
2024-05-18 20:03:43 +0200

    [+] multiplayer

---
+++ Assets/multiplayer/network_player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+
+    public PlayerController playerController;
+    public Cinemachine.CinemachineVirtualCamera playerCam;
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        playerController.enabled = IsOwner;
+        playerCam.Priority = IsOwner ? 1 : 0;
+    }
+}
