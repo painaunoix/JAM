295c865891320202ff6e77b6b737eddf6560cea1
Thomas-furstenberger <146249918+Thomas-furstenberger@users.noreply.github.com>
2024-05-18 22:12:21 +0200

    [+] add trans in game and score bord in main menu

--- Assets/main menu/src/scnemanager.cs
+++ Assets/main menu/src/scnemanager.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SceneChanger : MonoBehaviour
 {
+
+    public string sceneName;
+
     public void ChangeScene(string sceneName)
     {
+        StartCoroutine(Timer(sceneName));
+    }
+
+    IEnumerator Timer(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }
