e47de91b4a4db522d8d4f718e4b06a734529c723
nashiii <146207733+maxNashiii@users.noreply.github.com>
2024-05-18 22:13:53 +0200

    finish multiplayer

--- Assets/multiplayer/network.cs
+++ Assets/multiplayer/network.cs
@@ -1,10 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
+using System.Threading.Tasks;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Core;
 using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 
 public class host : MonoBehaviour
 {
+    private UnityTransport transport;
+    public TMPro.TMP_InputField JoinCodeInputField;
+
+    async void Awake() {
+        await Authenticate();
+    }
+
+    private static async Task Authenticate(){
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+    void Start() {
+        transport = FindObjectOfType<UnityTransport>();
+    }
+    public async void CreateMultiplayerRelay(){
+        Allocation a = await RelayService.Instance.CreateAllocationAsync(4);
+        JoinCodeInputField.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
+        transport.SetRelayServerData(a.RelayServer.IpV4, (ushort) a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public async void JoinSession(){
+        JoinAllocation a = await  RelayService.Instance.JoinAllocationAsync(JoinCodeInputField.text);
+        transport.SetClientRelayData(a.RelayServer.IpV4, (ushort) a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
+        NetworkManager.Singleton.StartClient();
+    }
     public void StartClient(){
         NetworkManager.Singleton.StartClient();
     }
