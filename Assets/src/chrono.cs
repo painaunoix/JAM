05f51d61853c2c1aadd179df0c159df95bd10237
nashiii <146207733+maxNashiii@users.noreply.github.com>
2024-05-18 11:09:33 +0200

    modifie chrono

--- Assets/src/chrono.cs
+++ Assets/src/chrono.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.Rendering;
 
 public class Chrono : MonoBehaviour
 {
@@ -10,10 +12,15 @@ public class Chrono : MonoBehaviour
     
     private float elapsedTime = 0f;
     private float minutes = 0f;
+    private float secondt = 0f;
+    private float minutes10 = 0f;
     private float nextUpdateTime = 0f;
     public TextMeshProUGUI textComponent;
     public TextMeshProUGUI text2;
 
+    public TextMeshProUGUI seconds10;
+
+    public TextMeshProUGUI minuteplus;
     void Start()
     {
     StartCoroutine(timer());
@@ -23,15 +30,23 @@ public class Chrono : MonoBehaviour
     {
         textComponent.text = Mathf.FloorToInt(elapsedTime).ToString();
         text2.text = Mathf.FloorToInt(minutes).ToString();
+        seconds10.text = Mathf.FloorToInt(secondt).ToString();
+        minuteplus.text = Mathf.FloorToInt(minutes10).ToString();
     }
 
 
     public IEnumerator timer()
     {
         yield return new WaitForSeconds(1);
-        if (elapsedTime == 60) {
-            elapsedTime = 0;
+        if (secondt == 5 && elapsedTime == 9)
             minutes++;
+        if (elapsedTime == 9) {
+            elapsedTime = -1;
+            secondt++;
+        }
+        if (minutes == 9) {
+            minutes = -1;
+            minutes10++;
         }
         elapsedTime++;
         StartCoroutine(timer());
