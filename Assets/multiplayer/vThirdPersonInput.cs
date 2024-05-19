8d757ee69a1dfb7dd3fa2a77859671f993bf0020
nashiii <146207733+maxNashiii@users.noreply.github.com>
2024-05-19 18:31:59 +0200

    fix input left right for solo player

--- Assets/multiplayer/vThirdPersonInput.cs
+++ Assets/multiplayer/vThirdPersonInput.cs
@@ -84,6 +84,7 @@ namespace Invector.vCharacterController
         public virtual void MoveInput()
         {
             cc.input.x = Input.GetAxis(horizontalInput);
+            cc.input.x *= -1;
             cc.input.z = Input.GetAxis(verticallInput);
         }
 
