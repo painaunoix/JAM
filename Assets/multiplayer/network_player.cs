bb35bc4f75da5ef257600ed302b9bb4cfd30b824
nashiii <146207733+maxNashiii@users.noreply.github.com>
2024-05-19 03:33:21 +0200

    fix multi

--- Assets/multiplayer/network_player.cs
+++ Assets/multiplayer/network_player.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class NetworkPlayer : NetworkBehaviour
 {
 
-  //  public CharacterController playerController;
+    public CharacterController playerController;
     public Cinemachine.CinemachineVirtualCamera playerCam;
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
-   //     playerController.enabled = IsOwner;
+       playerController.enabled = true;
+        playerController.enabled = IsOwner;
         playerCam.Priority = IsOwner ? 1 : 0;
+        playerController.enabled = false;
     }
 }
